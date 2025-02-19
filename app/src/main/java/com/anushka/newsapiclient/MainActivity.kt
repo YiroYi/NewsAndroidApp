40bf05fccd8562a7fbfebdda9713756ad1c2d963
Yiro Yi <yiroyi@live.com>
2025-02-18 22:56:20 -0600

    InjectingNewsAdapter

--- app/src/main/java/com/anushka/newsapiclient/MainActivity.kt
+++ app/src/main/java/com/anushka/newsapiclient/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.anushka.newsapiclient.databinding.ActivityMainBinding
+import com.anushka.newsapiclient.presentation.adapter.NewsAdapter
 import com.anushka.newsapiclient.presentation.viewmodel.NewsViewModel
 import com.anushka.newsapiclient.presentation.viewmodel.NewsViewModelFactory
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,6 +16,8 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var factory: NewsViewModelFactory
+    @Inject
+    lateinit var newsAdapter: NewsAdapter
     lateinit var viewModel: NewsViewModel
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
